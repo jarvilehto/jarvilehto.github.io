706737a92bc750aa704ba74b026d2995bd7867e4
J. <77263040+jarvilehto@users.noreply.github.com>
2023-11-25 16:38:31 +0200

    WIP: skill info

--- jarvilehto-app/app/page.tsx
+++ jarvilehto-app/app/page.tsx
@@ -1,6 +1,14 @@
+"use client";
 import Image from "next/image";
+import React from "react";
 
 export default function Home() {
+  const [skill, setSkill] = React.useState(false);
+
+  function changeCard() {
+    setSkill(!skill);
+  }
+
   return (
     <main className="min-h-screen w-full backdrop-blur-sm bg-blue-300/10 pb-6">
       <div className="flex flex-col items-center lg:hidden">
@@ -31,16 +39,38 @@ export default function Home() {
           </p>
         </div>
         <div className=" w-11/12 flex  flex-col pt-7 sm:flex-row sm:justify-center">
-          <div className="  mb-5  p-4 rounded bg-white/30 backdrop-blur-sm text-white shadow-xl sm:w-3/5 sm:mr-2 sm:mb-0">
-            <h1 className="font-bold underline">My skill set.</h1>
-            <br />
-            <p>
-              During my studies I have learned the basics of programming that
-              later evolved into being specialized in web-development and mobile
-              applications. I have been involved in many projects that have b
-            </p>
+          <div
+            onClick={changeCard}
+            className="  mb-5  p-4 rounded bg-white/30 backdrop-blur-sm text-white shadow-xl sm:w-3/5 sm:mr-2 sm:mb-0
+             transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-105 hover:bg-indigo-500 duration-300 ..."
+          >
+            {!skill && (
+              <>
+                <h1 className="font-bold underline">My skill set.</h1>
+                <br />
+                <p>
+                  During my studies I have learned the basics of programming
+                  that later evolved into being specialized in web-development
+                  and mobile applications. Some of the languages I have used
+                  during my studies and personal development projects and work
+                  have been.
+                </p>
+                <p className="mt-1 font-bold">click me.</p>
+              </>
+            )}
+
+            {skill && (
+              <>
+                <div className="w-full">
+                  <div className="flex justify-around font-bold border-b-2 pb-2">
+                    <h1>Language </h1>
+                    <p>Skill</p>
+                  </div>
+                </div>
+              </>
+            )}
           </div>
-          <div className=" h-1/3 p-3 rounded bg-white/30 backdrop-blur-sm justify-between text-white shadow-xl flex sm:w-2/5 sm:ml-2 flex-col">
+          <div className=" h-3/5 p-3 rounded bg-white/30 backdrop-blur-sm justify-between text-white shadow-xl flex sm:w-2/5 sm:ml-2 flex-col">
             <div className="">
               <p></p>
             </div>
